e73fad49c02000b61143afc50b20ec3353330ca7
Asher Ferri <asherferri85@gmail.com>
2018-03-17 01:07:13 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,5 @@
+var takeANumber = function(katzDeli, newCustomer) {
+	if (katzDeli.length > 0);
+	katzDeli.push(newCustomer);
+	return ('Welcome, ' + newCustomer + '. You are number ' + katzDeli.length + ' in line.')
+}
